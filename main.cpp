8b33ae7d9eb06d7a4f827ff90ae4f5fdea6c8add
Jack Duggan <jacksdggn@gmail.com>
2025-10-13 12:40:54 +1030

    feat: implement kruskal's

--- main.cpp
+++ main.cpp
@@ -14,16 +14,46 @@ struct Edge {
     Edge(){};
 };
 
+struct CompDestroy { /* comparator for pq to compare edge weights */
+    bool operator()(Edge const &a, Edge const &b) {
+        return a.weight < b.weight;
+    }
+};
+
+struct CompBuild { /* comparator for pq to compare edge weights */
+    bool operator()(Edge const &a, Edge const &b) {
+        return a.weight > b.weight;
+    }
+};
+
+int find(std::vector<int> &parent, int index) {
+    if (parent[index] == index) return index;
+    return find(parent, parent[index]);
+}
+
+bool sameComponent(std::vector<int> &parent, int index1, int index2) {
+    return find(parent, index1) == find(parent, index2);
+}
+
+void unionSets(std::vector<int> &parent, int index1, int index2) {
+    int root1 = find(parent, index1); /* find root of sets */
+    int root2 = find(parent, index2);
+
+    if (root1 == root2) return; /* already in same set */
+
+    parent[root2] = root1;
+}
+
 struct Graph {
-    std::vector<std::vector<Edge>> edges;               /* adjacency list for Prim's */
-    std::vector<Edge> cost;                             /* edge list for Kruskal's */
-    int nvertices;                                      /* number of vertices in graph */
-    int nedges;                                         /* number of edges in graph */
-    bool directed;                                      /* is the graph directed */
-    void insertEdge(int, int);                          /* insert edge, nodes are 1 indexed */
+    std::vector<std::vector<Edge>> edges;                           /* adjacency list for Prim's */
+    std::priority_queue<Edge, std::vector<Edge>, CompBuild> cost;   /* edge list for Kruskal's */
+    int nvertices;                                                  /* number of vertices in graph */
+    int nedges;                                                     /* number of edges in graph */
+    bool directed;                                                  /* is the graph directed */
+    void insertEdge(int, int);                                      /* insert edge, nodes are 1 indexed */
     
-    Graph(bool d) : directed(d){};                      /* constructor */
-    void readGraph();                                   /* read graph from standard input */
+    Graph(bool d) : directed(d){};                                  /* constructor */
+    void readGraph();                                               /* read graph from standard input */
 };
 
 int convert(char c) { /* helper function to convert char value to edge weight */
@@ -31,12 +61,6 @@ int convert(char c) { /* helper function to convert char value to edge weight */
     if (x < 26) return x;
     return x - 6;
 }
-
-struct CompDestroy { /* comparator for pq to compare edge weights */
-    bool operator()(Edge const &a, Edge const &b) {
-        return a.weight < b.weight;
-    }
-};
 
 void Graph::readGraph() {
     /* expected input format is twoo strings representing edge matrix
@@ -59,7 +83,7 @@ void Graph::readGraph() {
                 edges[j].push_back(Edge(j, i, destroy));
             }
             build = convert(b[index]);
-            cost.push_back(Edge(i, j, build));
+            cost.push(Edge(i, j, build));
         }
     }
 }
@@ -86,8 +110,7 @@ int primModified(std::vector<bool> &inTree, std::vector<int> &parent, Graph &g, 
         pq.pop();
         if (!inTree[vertex.y]) {
             inTree[vertex.y] = true;
-            parent[vertex.y] = vertex.x;
-            std::cout << "edge " << vertex.x << "->" << vertex.y << " added" << std::endl;
+            parent[ find(parent, vertex.y) ] = find(parent, vertex.x);
             for (auto edge : g.edges[vertex.y]) {
                 if (!inTree[edge.y] && distance[edge.y] < edge.weight) {
                     pq.push(edge);
@@ -112,16 +135,22 @@ int main() {
     std::vector<bool> inTree(g.nvertices, false);
     std::vector<int> parent(g.nvertices);
     std::iota(parent.begin(), parent.end(), 0);
-    int comp = 1;
     int cost = 0;
 
     for (int i = 0; i < g.nvertices; i++) {
         if (!inTree[i]) {
-            std::cout << "Component " << comp << ":" << std::endl;
-            comp++;
             cost += primModified(inTree, parent, g, i);
         }
     }
+    Edge edge;
+    while (!g.cost.empty()) {
+        edge = g.cost.top();
+        g.cost.pop();
+        if (!sameComponent(parent, edge.x, edge.y)) {
+            cost += edge.weight;
+            unionSets(parent, edge.x, edge.y);
+        }
+    }
     
-    std::cout << "cost: " << cost << std::endl;
+    std::cout << cost << std::endl;
 }
